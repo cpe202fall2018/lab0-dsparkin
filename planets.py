7fba71cb5c634ad8ea8bd8ec1d4adf2c8f87cabb
dsparkin <dsparkin@calpoly.edu>
2018-09-24 17:34:46 -0700

    submission

--- planets.py
+++ planets.py
@@ -1,7 +1,7 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    # write your code here
+    wt_earth = float(input("What do you weigh on earth? "))
+    print("\nOn Mars you would weigh {0:.2f} pounds.\nOn Jupiter you would weigh {1:.2f} pounds.".format(wt_earth*0.38,wt_earth*2.34))
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
--- planets_tests.py
+++ planets_tests.py
@@ -10,8 +10,7 @@ class Test_planets(unittest.TestCase):
         expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
         weight_on_planets()
         #print(student_output.getvalue().strip())
-        self.assertEqual(expected_out, student_output.getvalue().strip())
-
+        self.assertEqual(expected_out.strip(), student_output.getvalue().strip())
 
 if __name__ == "__main__":
         unittest.main()
